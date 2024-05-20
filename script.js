09b3776d3ed53c8b109c4242f9aa8097c12e00a1
Pratyush R <113253685+pratyushxr@users.noreply.github.com>
2024-05-20 11:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,41 @@
-// let url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+document.addEventListener("DOMContentLoaded", function () {
+    const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+    const key = "97ed86b99fdcf738c7a080e0fa9fde20";
 
-// let key = "&appid=0318b21fe0c0ad7d4d2c4c74d1e7df49";
+    const temp = document.querySelector(".temp");
+    const cityname = document.querySelector(".cityname");
+    const searchBtn = document.querySelector(".searchbtn");
+    const subBtn = document.querySelector(".subBtn"); // Corrected class name here
+    const humidity = document.querySelector(".humidity");
+    const wind = document.querySelector("#wind");
 
+    console.log(temp); 
 
-// let temp = document.querySelector(".temp");
-// let cityname = document.querySelector(".cityname");
-// let searchBtn = document.querySelector(".searchbtn");
-// let subBtn = document.querySelector("button");
-// let humidity = document.querySelector(".humidity");
-// let wind = document.querySelector("#wind");
+    subBtn.addEventListener("click", () => {
+        let city = searchBtn.value;
+        console.log(city);
+        checkWeather(city);
+    });
 
-// console.log(temp); 
+    async function checkWeather(cityarg) {
+        try {
+            let response = await fetch(url + cityarg + "&appid=" + key);
+            let data = await response.json();
 
-// subBtn.addEventListener("click", () => {
-//   city = searchBtn.value;
-//   console.log(city);
-  
-//   return checkWeather(city);
-// });
-
-// async function checkWeather(cityarg) {
-//   try {
-//     let data = await fetch(url + cityarg + key);
-//     let res = await data.json();
-//     result = res;
-//     cityname.innerHTML = cityarg;
-//     console.log(result);
-//     temp.innerHTML = Math.round(result.main.temp) + "`o C";
-//     humidity.innerHTML = Math.round(result.main.humidity) + "%";
-//     wind.innerHTML = (result.wind.speed + " km/h");
-
-//   } catch (e) {
-//     console.log("Error" + e);
-//   };
-// };
-
-// checkWeather();
-
-
-
-let url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-let key = "&appid=0318b21fe0c0ad7d4d2c4c74d1e7df49";
-
-let temp = document.querySelector(".temp");
-let cityname = document.querySelector(".cityname");
-let searchBtn = document.querySelector(".searchbtn");
-let subBtn = document.querySelector(".subBtn");
-let humidity = document.querySelector(".humidity");
-let wind = document.querySelector("#wind");
-
-console.log(temp); 
-
-subBtn.addEventListener("click", () => {
-  let city = searchBtn.value;
-  console.log(city);
-  
-  checkWeather(city);
-});
-
-async function checkWeather(cityarg) {
-  try {
-    let response = await fetch(url + cityarg + key);
-    let data = await response.json();
-    
-    if (data.cod === 200) { // Check if the response is successful
-      cityname.innerHTML = cityarg;
-      temp.innerHTML = Math.round(data.main.temp) + "°C";
-      humidity.innerHTML = Math.round(data.main.humidity) + "%";
-      wind.innerHTML = data.wind.speed + " km/h";
-    } else {
-      console.log("City not found");
-      cityname.innerHTML = "City not found";
-      temp.innerHTML = "";
-      humidity.innerHTML = "";
-      wind.innerHTML = "";
+            if (data.cod === 200) { // Check if the response is successful
+                cityname.innerHTML = data.name + ", " + data.sys.country;
+                temp.innerHTML = Math.round(data.main.temp) + "°C"; // Corrected temperature display
+                humidity.innerHTML = "Humidity: " + Math.round(data.main.humidity) + "%";
+                wind.innerHTML = "Wind Speed: " + data.wind.speed + " km/h";
+            } else {
+                console.log("City not found");
+                cityname.innerHTML = "City not found";
+                temp.innerHTML = "";
+                humidity.innerHTML = "";
+                wind.innerHTML = "";
+            }
+        } catch (e) {
+            console.log("Error: " + e);
+        }
     }
-  } catch (e) {
-    console.log("Error: " + e);
-  }
-}
-
-// checkWeather(); // Uncomment if you want to call the function on page load with a default city
+});
